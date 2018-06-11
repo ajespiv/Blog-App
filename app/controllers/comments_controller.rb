95596b5910d9cb89b986bffac5b3da313d99fd40
Alexis Espinoza <ajespiv@gmail.com>
2018-06-11 10:23:56 -0400

    Index section update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,6 @@
 class CommentsController < ApplicationController
 
-  http_basic_authenticate_with name: "alexis", password: "secret", only: :destroy
+  http_basic_authenticate_with name: "", password: "", only: :destroy
 
   def create
     @article = Article.find(params[:article_id])
